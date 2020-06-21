95ad51ec9b6d0e105d040f9cce83d863d3fcb087
sabrina <sabrina.shaik80@gmail.com>
2020-06-21 20:06:56 +0530

    version 1

---
+++ main_app/config.py
@@ -0,0 +1,5 @@
+consumer_key = '945854006-RGdFNpdBbXRLjk7ir4INSVutvuFS5sYPYSFY22Ll'
+consumer_secret = 'TRiWvIJqBQdOrmu1Yz3EXHj9gD6x7xSheNRcCXRyRKSuf'
+
+access_token  = '5PbS87u5bnbPNnlOaru6ujWkK'
+access_token_secret = '1bQAMFQo9JpmdKtQJDP3FG250PtTN6PDQin4XcKoh5i3mh8u3T'
